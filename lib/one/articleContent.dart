188147855f9491205325f8029c472a6dd8269721
j787701730 <787701730@qq.com>
2019-03-22 21:41:28 +0800

    美化one 文章详情

--- lib/one/articleContent.dart
+++ lib/one/articleContent.dart
@@ -71,101 +71,192 @@ class _ArticleContentState extends State<ArticleContent> {
           : ListView(
               children: <Widget>[
                 Container(
-                  child: Text(article['hp_title']),
+                  padding: EdgeInsets.only(top: 20, bottom: 20, left: 20),
+                  child: Text(
+                    article['hp_title'],
+                    style: TextStyle(fontSize: 22),
+                  ),
                 ),
                 Container(
+                  padding: EdgeInsets.only(left: 20),
                   child: Text('文 / ${article['author'][0]['user_name']}'),
                 ),
                 Html(
                   data: article['hp_content'],
-                  padding: EdgeInsets.all(8),
-                ),
-                Container(
-                  child: Text(article['hp_author_introduce']),
-                ),
-                Container(
-                  child: Text(article['editor_email']),
-                ),
-                Container(
-                  child: Text(
-                    '作者',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  defaultTextStyle: TextStyle(
+                    height: 1.5,
+                    fontSize: 16,
                   ),
                 ),
-                Column(
-                  children: article['author_list'].map<Widget>((list) {
-                    return Row(
-                      children: <Widget>[
-                        SizedBox(
-                          width: 50,
-                          child: Image.network(
-                            '${list['web_url']}',
-                            fit: BoxFit.fitWidth,
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Container(
-                                child: Text(list['user_name']),
-                              ),
-                              Container(
-                                child: Text(list['desc']),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    );
-                  }).toList(),
+                Container(
+                  padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                  child: Text(
+                    '${article['hp_author_introduce']} ${article['editor_email']}',
+                    style: TextStyle(color: Colors.black38),
+                  ),
                 ),
                 Container(
+                  padding: EdgeInsets.only(left: 20, right: 20, bottom: 10),
+                  child: Text(
+                    '作者',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                      height: 4,
+                      width: 60,
+                      color: Colors.black87,
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: article['author_list'].map<Widget>((list) {
+                      return Row(
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)), color: Colors.black),
+                            width: 44,
+                            height: 44,
+                            child: ClipOval(
+                              child: Image.network(
+                                '${list['web_url']}',
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Container(
+                                    child: Text(
+                                      list['user_name'],
+                                      style: TextStyle(height: 1.5),
+                                    ),
+                                  ),
+                                  Container(
+                                    child: Text(list['desc'], style: TextStyle(height: 1.5)),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    }).toList(),
+                  ),
+                ),
+                Container(
+                  height: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 20, right: 20, bottom: 10),
                   child: Text(
                     '评论列表',
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                      height: 4,
+                      width: 60,
+                      color: Colors.black87,
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 20,
                 ),
                 comments.isEmpty
                     ? Placeholder(
                         fallbackHeight: 1,
                         color: Colors.transparent,
                       )
-                    : Column(
-                        children: comments['data'].map<Widget>((comment) {
-                          return Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Row(
+                    : Container(
+                        padding: EdgeInsets.only(left: 20, right: 20,top: 10,bottom: 10),
+                        child: Column(
+                          children: comments['data'].map<Widget>((comment) {
+                            return Container(
+                              padding: EdgeInsets.only(top: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: <Widget>[
-                                  SizedBox(
-                                    width: 40,
-                                    child: Image.network(comment['user']['web_url']),
+                                  Row(
+                                    children: <Widget>[
+                                      Container(
+                                        decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)), color: Colors.black),
+                                        width: 20,
+                                        height: 20,
+                                        child: ClipOval(
+                                          child: Image.network(
+                                            '${comment['user']['web_url']}',
+                                            fit: BoxFit.contain,
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Container(
+                                          padding: EdgeInsets.only(left: 10),
+                                          child: Text(comment['user']['user_name']),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        child: Text(comment['input_date']),
+                                      )
+                                    ],
                                   ),
-                                  Expanded(
-                                    child: Text(comment['user']['user_name']),
+                                  Container(
+                                    padding: EdgeInsets.all(20),
+                                    child: Text(comment['content']),
                                   ),
-                                  SizedBox(
-                                    child: Text(comment['input_date']),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.thumb_up,
+                                        color: Colors.black38,
+                                        size: 16,
+                                      ),
+                                      Text(' ${comment['praisenum']}',style: TextStyle(
+                                          color: Colors.black38
+                                      ),)
+                                    ],
+                                  ),
+                                  Container(
+//                                  height: 1,
+//                                  color: Colors.black38,
+                                    padding: EdgeInsets.only(top: 10),
+                                    decoration: BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                color: Colors.black12
+                                            )
+                                        )
+                                    ),
                                   )
                                 ],
                               ),
-                              Container(
-                                child: Text(comment['content']),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: <Widget>[
-                                  Icon(
-                                    Icons.thumb_up,
-                                    color: Colors.black38,
-                                  ),
-                                  Text('${comment['praisenum']}')
-                                ],
-                              )
-                            ],
-                          );
-                        }).toList(),
+                            );
+                          }).toList(),
+                        ),
                       )
               ],
             ),
